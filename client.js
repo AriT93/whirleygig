6bbf3b2b8f7666c794ba20f4800d04ce704fee49
AriT93 <arit93@yahoo.com>
2010-12-01 15:55:04 -0600

    pre pull committ

--- client.js
+++ client.js
@@ -4,10 +4,13 @@ var net = require('net'),
     message = require('Message'),
     sys = require('sys');
 
+var m =  new message.Message();
+var m2 = new message.Message();
+
 var stream  = new net.Stream();
 stream.setEncoding('utf-8');
 var streamdata = stream.addListener("data", function(data){
-//    console.log(data);
+    //    console.log(data);
 });
 
 stream.addListener("end",function(){
@@ -22,26 +25,30 @@ stream.connect(8000, host="127.0.0.1");
 function pushDatatoStream(){
     counter +=1 ;
     var data = new Array();
-    var m =  new message.Message();
+    m = new message.Message();
     var mlistener = m.addListener('update',function(data){
-        m.id = data;
+        if(! m.id){
+            m.id = data;
+        }
     });
-    var m2 = new message.Message();
+    m.text = "data";
+    m.save();
+    data.push(m);
+    m2 = new message.Message();
     var m2listener = m2.addListener('update',function(data){
-        m2.id = data;
+        if(! m2.id){
+            m2.id = data;
+        }
     });
     m2.text = "stream";
     m2.save();
     data.push(m2);
-//    console.log(m.id);
-    m.text = "data";
-    m.save();
-    data.push(m);
+    //    console.log(m.id);
     stream.write(JSON.stringify(data));
     if(counter > 10){
-    clearInterval(pdinterval);
-    stream.end();
-    //process.exit();
+        clearInterval(pdinterval);
+        stream.end();
+        //process.exit();
     }
     // console.log(pdinterval);
     // clearInterval(pdinterval);
